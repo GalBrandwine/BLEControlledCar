86868bee2fe2d19dcb289d35dca563597c28d768
Gal Brandwine <gal080592@gmail.com>
2022-12-27 01:37:45 +0200

    Done

--- BLERCcar/BLERCcar_server/Motor.hpp
+++ BLERCcar/BLERCcar_server/Motor.hpp
@@ -16,49 +16,55 @@ namespace car
         int Pin2{-1};
 
         Motor(){};
-        Motor(int pin1, int pin2, int enable1Pin, int pwmChannel)
-            : Pin1(pin1),
-              Pin2(pin2),
-              m_Enable1Pin(enable1Pin),
-              m_PwmChannel(pwmChannel)
-
-        {
-            Serial.printf("Motor constructing:\n");
-            Serial.printf("Pin1=%d\n", Pin1);
-            Serial.printf("Pin2=%d\n", Pin2);
-            Serial.printf("Enable1Pin=%d\n", m_Enable1Pin);
-            Serial.printf("PwmChannel=%d\n", m_PwmChannel);
-            // sets the pins as outputs:
-            pinMode(Pin1, OUTPUT);
-            pinMode(Pin2, OUTPUT);
-            pinMode(m_Enable1Pin, OUTPUT);
-
-            // configure LED PWM functionalities
-            ledcSetup(m_PwmChannel, m_Freq, m_Resolution);
-
-            // attach the channel to the GPIO to be controlled
-            ledcAttachPin(m_Enable1Pin, m_PwmChannel);
-            ledcWrite(m_PwmChannel, m_Pwmduty);
-        };
-
-        void SetSpeed(int speed)
-        {
-            m_Pwmduty = speedPercentageToDutyCycle(speed);
-            Serial.println(__PRETTY_FUNCTION__);
-            Serial.printf("Input speed [%d]%%\n", speed);
-            Serial.printf("Setting [%d] pwmduty\n", m_Pwmduty);
-            ledcWrite(m_PwmChannel, m_Pwmduty);
-        }
-
-        int speedPercentageToDutyCycle(int speed)
-        {
-            return map(speed, 0, 100, 0, 255);
-        }
-        int CurrentSpeedPercentage()
-        {
-            Serial.println(__PRETTY_FUNCTION__);
-            Serial.printf("Pwmduty: %d\n", m_Pwmduty);
-            return map(m_Pwmduty, 0, 255, 0, 100);
-        }
+        Motor(int pin1, int pin2, int enable1Pin, int pwmChannel);
+        void SetSpeed(int speed);
+        int speedPercentageToDutyCycle(int speed);
+        int CurrentSpeedPercentage();
     };
+
+    Motor::Motor(){};
+    Motor::Motor(int pin1, int pin2, int enable1Pin, int pwmChannel)
+        : Pin1(pin1),
+          Pin2(pin2),
+          m_Enable1Pin(enable1Pin),
+          m_PwmChannel(pwmChannel)
+
+    {
+        Serial.printf("Motor constructing:\n");
+        Serial.printf("Pin1=%d\n", Pin1);
+        Serial.printf("Pin2=%d\n", Pin2);
+        Serial.printf("Enable1Pin=%d\n", m_Enable1Pin);
+        Serial.printf("PwmChannel=%d\n", m_PwmChannel);
+        // sets the pins as outputs:
+        pinMode(Pin1, OUTPUT);
+        pinMode(Pin2, OUTPUT);
+        pinMode(m_Enable1Pin, OUTPUT);
+
+        // configure LED PWM functionalities
+        ledcSetup(m_PwmChannel, m_Freq, m_Resolution);
+
+        // attach the channel to the GPIO to be controlled
+        ledcAttachPin(m_Enable1Pin, m_PwmChannel);
+        ledcWrite(m_PwmChannel, m_Pwmduty);
+    };
+
+    void Motor::SetSpeed(int speed)
+    {
+        m_Pwmduty = speedPercentageToDutyCycle(speed);
+        Serial.println(__PRETTY_FUNCTION__);
+        Serial.printf("Input speed [%d]%%\n", speed);
+        Serial.printf("Setting [%d] pwmduty\n", m_Pwmduty);
+        ledcWrite(m_PwmChannel, m_Pwmduty);
+    }
+
+    int Motor::speedPercentageToDutyCycle(int speed)
+    {
+        return map(speed, 0, 100, 0, 255);
+    }
+    int Motor::CurrentSpeedPercentage()
+    {
+        Serial.println(__PRETTY_FUNCTION__);
+        Serial.printf("Pwmduty: %d\n", m_Pwmduty);
+        return map(m_Pwmduty, 0, 255, 0, 100);
+    }
 } // car
